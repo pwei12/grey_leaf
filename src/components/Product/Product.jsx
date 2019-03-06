72d98111bc384fbba606a21aa99805b09cef6bf2
pwei12 <wpwei_16@yahoo.com>
2019-03-07 07:03:26 +0800

    Enable 'Add to Cart' button in Products page to add item to shopping cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
+import { addToCart } from '../../services/productListService'
 
 function Product({ product }) {
   const { name, price, imageUrl, id } = product;
+  const [addCartButton, setAddCartButton] = useState(false);
+
+  const handleAddToCart = e => {
+    e.preventDefault();
+    setAddCartButton(!addCartButton);
+    addToCart(id, 1);
+  };
+
   return (
     <Card className="mb-3">
       <Link to={`/products/product/${id}`}>
@@ -13,7 +22,7 @@ function Product({ product }) {
         <Card.Title>{name}</Card.Title>
         <Card.Text>${price.toFixed(2)}</Card.Text>
         {/* <Card.Text>{description}</Card.Text> */}
-        <Button variant="primary" >
+        <Button variant="primary" onClick={handleAddToCart}>
           <Link to="/cart" className="text-white">Add to Cart</Link>
         </Button>
       </Card.Body>
