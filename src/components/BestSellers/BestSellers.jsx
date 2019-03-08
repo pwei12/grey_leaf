8975a12a92a1848e4c1ecfbda2291990053a0319
pwei12 <wpwei_16@yahoo.com>
2019-03-08 12:36:30 +0800

    Remove redundant lines in BestSellers

--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -2,30 +2,6 @@ import React from "react";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
 
 function BestSellers() {
-
-  //import product list service
-  // call your getProductList()
-  // renderBestSeller = foundList.filter((item) => {
-      // return true or false based of best seller
-  //}).map((item)=>{
-    //   <Carousel.Item>
-    //   <img
-    //     className="d-block w-100"
-    //     src="https://images.unsplash.com/photo-1549404183-c6ce67f5697c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=clip&max-h=300&q=60"
-    //     alt="Soap ABD"
-    //   />
-    //   <Carousel.Caption>
-    //     <h3>Soap AbC</h3>
-    //     <p>
-    //       Praesent commodo cursus magna, vel scelerisque nisl
-    //       consectetur.
-    //     </p>
-    //   </Carousel.Caption>
-    // </Carousel.Item>
-  // })  
-
-
-
   return (
     <Container fluid>
       <Row className="justify-content-md-center">
@@ -36,7 +12,6 @@ function BestSellers() {
       <Row className="justify-content-md-center">
         <Col md={4} xs={12} sm={6}>
           <Carousel>
-            {/* {renderBestSeller} */}
             <Carousel.Item>
               <img
                 className="d-block w-100"
