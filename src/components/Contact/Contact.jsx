06ea440357cc7ca87f151ad9cf0f84e0681b796f
pwei12 <wpwei_16@yahoo.com>
2019-03-09 19:57:04 +0800

    Change wording in contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 function Contact() {
   return (
     <Container className="mt-5">
-        <h3>Contact:</h3>
-      <a href="https://www.facebook.com/greyleafcleaningproduct" target="_blank" rel="noopener noreferrer"> Facebook </a>
-      <a href="https://www.instagram.com/leafgrey/" target="_blank" rel="noopener noreferrer"> Instagram </a>
-          <p>
-              WhatApps: 019-2781910 (Cynthia)
-          </p>
+      <h3>Contact:</h3>
+      <p>
+        Visit our
+        <a
+          href="https://www.facebook.com/greyleafcleaningproduct"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Facebook{" "}
+        </a>
+        page or
+      <a
+        href="https://www.instagram.com/leafgrey/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        Instagram
+      </a>
+        .
+      </p>
+      <p>WhatApps: 019-2781910 (Cynthia)</p>
     </Container>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
