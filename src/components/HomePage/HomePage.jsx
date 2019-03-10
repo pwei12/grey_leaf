f4316ce7683bd396d337c4fa03db86b764c09ed9
pwei12 <wpwei_16@yahoo.com>
2019-03-11 06:08:56 +0800

    Change HomePage from class component to function component

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,31 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import BestSellers from "../BestSellers/BestSellers";
 
-export class HomePage extends Component {
-  render() {
-    return (
-      <div className="pb-5">
-        <div>
-          <BestSellers />
-        </div>
-        <Container className="mt-5">
-          <Row className="justify-content-center">
-            <Col sm="auto">
-              <h1 className="justify-content-center">
-                <strong>
-                  Welcome to <em>Grey Leaf</em>
-                </strong>
-              </h1>
-              <p>~ Our products are 100% handmade. <br/>
-                 ~ We welcome custom order 
-              </p>
-            </Col>
-          </Row>
-        </Container>
+function HomePage() {
+  return (
+    <div className="pb-5">
+      <div>
+        <BestSellers />
       </div>
-    );
-  }
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <Col sm="auto">
+            <h1 className="justify-content-center">
+              <strong>
+                Welcome to <em>Grey Leaf</em>
+              </strong>
+            </h1>
+            <p>
+              ~ Our products are 100% handmade. <br />~ We welcome custom order
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default HomePage;
