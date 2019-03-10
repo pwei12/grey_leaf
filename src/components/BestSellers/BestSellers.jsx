b3ac73c4079563de735b064b8ba177ad4d986c62
pwei12 <wpwei_16@yahoo.com>
2019-03-11 06:57:46 +0800

    Link BestSeller's images to product details page; get data from service instead of hard code

--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -1,62 +1,41 @@
 import React from "react";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
+import { getAllProducts } from "../../services/productListService";
+import { Link } from "react-router-dom";
 
 function BestSellers() {
+  const bestSellerList = getAllProducts().filter(product => product.bestSeller);
   return (
-    <Container fluid>
-      <Row className="justify-content-center">
-        <Col xs={6} sm="auto">
-          <h3>Best Sellers</h3>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col xs={12} sm={8} md={6}>
-          <Carousel>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1549404183-c6ce67f5697c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=clip&max-h=300&q=60"
-                alt="Soap ABD"
-              />
-              <Carousel.Caption>
-                <h3>Soap AbC</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1549404183-c6ce67f5697c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=clip&max-h=300&q=60"
-                alt="Soap XYZ"
-              />
-              <Carousel.Caption>
-                <h3>Soap XYZ</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1549404183-c6ce67f5697c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=clip&max-h=300&q=60"
-                alt="Soap JKL"
-              />
-              <Carousel.Caption>
-                <h3>Soap JKL</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <Container fluid>
+        <Row className="justify-content-center mt-3">
+          <Col xs={6} sm="auto">
+            <h3>Best Sellers</h3>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={12} sm={8} md={6}>
+            <Carousel>
+              {bestSellerList.map((product, index) => (
+                <Carousel.Item key={index}>
+                  <Link to={`/products/product/${product.id}`}>
+                    <img
+                      className="d-block w-100"
+                      src={product.imageUrl}
+                      alt={product.name}
+                    />
+                    <Carousel.Caption>
+                      <h3>{product.name}</h3>
+                      <p>${product.price.toFixed(2)}</p>
+                    </Carousel.Caption>
+                  </Link>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
