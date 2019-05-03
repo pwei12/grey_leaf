f9b5c192114083ae863adb2c83dd18d5ac20cddc
pwei12 <wpwei-16@yahoo.com>
2019-05-03 17:39:50 +0800

    temporary change to force request to send production backend

--- src/services/serverService.js
+++ src/services/serverService.js
@@ -2,9 +2,10 @@
 const getServerUrl = path => {
   const isProdEnv = process.env.NODE_ENV === "production";
   console.log(process.env.NODE_ENV);
-  return isProdEnv
-    ? `https://grey-leaf-api.herokuapp.com/${path}`
-    : `http://localhost:8080/${path}`;
+    return `https://grey-leaf-api.herokuapp.com/${path}`
+  // return isProdEnv
+  //   ? `https://grey-leaf-api.herokuapp.com/${path}`
+  //   : `http://localhost:8080/${path}`;
 };
 
 module.exports = getServerUrl;
