1fcf78aa1a257a00e05e6e6c60167e6808a7b773
pwei12 <wpwei-16@yahoo.com>
2019-05-03 16:09:55 +0800

    Temporary debug issue with NODE_ENV

--- src/services/serverService.js
+++ src/services/serverService.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const getServerUrl = path => {
   const isDevEnv = process.env.NODE_ENV !== "production";
+  console.log("Node Env is")
+  console.log(process.env.NODE_ENV)
   return isDevEnv
     ? `http://localhost:8080/${path}`
     : `https://grey-leaf-api.herokuapp.com/${path}`;
