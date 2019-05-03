4b4f9b563d40498768d07b12fb47df9ca68844f8
pwei12 <wpwei-16@yahoo.com>
2019-05-03 17:09:14 +0800

    Remove unrequired dotenv

--- src/services/serverService.js
+++ src/services/serverService.js
@@ -1,12 +1,10 @@
-// require("dotenv").config();
 
 const getServerUrl = path => {
-
   const isProdEnv = process.env.NODE_ENV === "production";
+  console.log(process.env.NODE_ENV);
   return isProdEnv
     ? `https://grey-leaf-api.herokuapp.com/${path}`
     : `http://localhost:8080/${path}`;
-
 };
 
 module.exports = getServerUrl;
