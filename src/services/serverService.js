f80239d4bd98d03e72654f8c7591d1b030355a28
pwei12 <wpwei-16@yahoo.com>
2019-05-03 16:18:51 +0800

    Modify server url to always use prod url for debugging

--- src/services/serverService.js
+++ src/services/serverService.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
 const getServerUrl = path => {
-  const isDevEnv = process.env.NODE_ENV !== "production";
-  console.log("Node Env is")
-  console.log(process.env.NODE_ENV)
-  return isDevEnv
-    ? `http://localhost:8080/${path}`
-    : `https://grey-leaf-api.herokuapp.com/${path}`;
+  return `https://grey-leaf-api.herokuapp.com/${path}`
+
+  // const isDevEnv = process.env.NODE_ENV !== "production";
+  // return isDevEnv
+  //   ? `http://localhost:8080/${path}`
+  //   : `https://grey-leaf-api.herokuapp.com/${path}`;
+  
 };
 
 module.exports = getServerUrl;
